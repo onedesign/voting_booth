6b8b909686b22cc1a22f16a1208ee69f1a21e329
Dan Hodos <danhodos@gmail.com>
2011-02-04 21:13:13 -0600

    Adds ability for a voter to vote on a votable.

--- lib/voting_engine/voter.rb
+++ lib/voting_engine/voter.rb
@@ -4,11 +4,24 @@ module VotingEngine
 
     module ClassMethods
       def voter(opts={})
+        include VoterInstanceMethods
+
         has_many :votes, :as => :voter
         has_many :votes_for, :as => :voter, :conditions => {:for => true}, :class_name => "Vote"
         has_many :votes_against, :as => :voter, :conditions => {:for => false}, :class_name => "Vote"
       end
     end
+
+    module VoterInstanceMethods
+      def vote(votable, value)
+        votes.find_or_initialize_by_votable_type_and_votable_id(votable.class.to_s, votable.id).tap do |vote|
+          vote.update_attribute(:for, value)
+        end
+      end
+
+      def vote_for(votable); vote(votable, true); end
+      def vote_against(votable); vote(votable, false); end
+    end # VoterInstanceMethods
   end # Voter
 end # VotingEngine
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,5 +4,29 @@ describe User do
   it { should have_many(:votes) }
   it { should have_many(:votes_for) }
   it { should have_many(:votes_against) }
-end
 
+  context "voting" do
+    let(:votable) { mock_model(BlogPost) }
+    let(:vote) { mock_model(Vote) }
+    let(:vote_proxy) { mock('vote proxy') }
+
+    it "should create or update Vote" do
+      subject.id = 1
+      subject.should_receive(:votes).and_return(vote_proxy)
+      vote_proxy.should_receive(:find_or_initialize_by_votable_type_and_votable_id).with("BlogPost", votable.id).and_return(vote)
+      vote.should_receive(:update_attribute).with(:for, true)
+
+      subject.vote(votable, true)
+    end
+
+    it "should allow voting for" do
+      subject.should_receive(:vote).with(votable, true)
+      subject.vote_for(votable)
+    end
+
+    it "should allow voting against" do
+      subject.should_receive(:vote).with(votable, false)
+      subject.vote_against(votable)
+    end
+  end
+end
