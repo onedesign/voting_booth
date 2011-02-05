2404bfd7a9eb5af12b8e315e093264040df67421
Dan Hodos <danhodos@gmail.com>
2011-02-05 10:54:29 -0600

    Adds named scopes for finding Votes by votable, voter, or both.

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,4 +3,16 @@ class Vote < ActiveRecord::Base
   belongs_to :votable, :polymorphic => true, :counter_cache => true
 
   validates_presence_of :votable, :voter
+
+  scope :for_votable, lambda {|votable|
+    where :votable_type => votable.class.to_s, :votable_id => votable.id
+  }
+
+  scope :for_voter, lambda {|voter|
+    where :voter_type => voter.class.to_s, :voter_id => voter.id
+  }
+
+  scope :for_voter_and_votable, lambda {|voter,votable|
+    for_votable(votable).for_voter(voter).limit(1)
+  }
 end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -6,4 +6,30 @@ describe Vote do
 
   it { should validate_presence_of(:voter) }
   it { should validate_presence_of(:votable) }
+
+  context "scopes" do
+    let(:votable) { mock_model(BlogPost) }
+    let(:voter) { mock_model(User) }
+    let(:vote) { mock_model(Vote) }
+
+    it "should find for_votable" do
+      where_hash = { :votable_type => 'BlogPost', :votable_id => votable.id }
+      Vote.for_votable(votable).where_values_hash.should == where_hash
+    end
+
+    it "should find for_voter" do
+      where_hash = { :voter_type => 'User', :voter_id => voter.id }
+      Vote.for_voter(voter).where_values_hash.should == where_hash
+    end
+
+    it "should find for_voter_and_votable" do
+      where_hash = Vote.for_voter(voter).where_values_hash
+      where_hash.merge!(Vote.for_votable(votable).where_values_hash)
+      Vote.for_voter_and_votable(voter, votable).where_values_hash.should == where_hash
+    end
+
+    it "should only return a single item for_voter_and_votable" do
+      Vote.for_voter_and_votable(voter, votable).limit_value.should == 1
+    end
+  end
 end
