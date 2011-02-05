d41622e0881680b4b2055cc403b71b9dc60b9a59
Dan Hodos <danhodos@gmail.com>
2011-02-05 13:01:53 -0600

    Creates generators based on migrations in dummy app

---
+++ lib/generators/voting_engine/votable_generator.rb
@@ -0,0 +1,22 @@
+require 'rails/generators'
+require 'rails/generators/migration'
+require 'rails/generators/active_record'
+
+module VotingEngine
+  class VotableGenerator < Rails::Generator::NamedBase
+    desc "Makes a specified model, e.g. BlogPost, votable."
+
+    include Rails::Generators::Migration
+
+    source_root File.expand_path('../templates', __FILE__)
+
+    class << self
+      delegate :next_migration_number, :to => ActiveRecord::Generators::Base
+    end
+
+    def create_migration_file
+      migration_template "votable_migration.rb.erb", "db/migrate/make_#{plural_name}_votable.rb"
+    end
+  end # VotableGenerator
+end # VotingEngine
+
---
+++ lib/generators/voting_engine/votes_generator.rb
@@ -0,0 +1,19 @@
+require 'rails/generators'
+require 'rails/generators/migration'
+require 'rails/generators/active_record'
+
+module VotingEngine
+  class VotesGenerator < Rails::Generator::Base
+    desc "Creates the Votes table. Only needs to be run once."
+    include Rails::Generators::Migration
+    source_root File.expand_path('../templates', __FILE__)
+
+    class << self
+      delegate :next_migration_number, :to => ActiveRecord::Generators::Base
+    end
+
+    def create_migration_file
+      migration_template "votes_migration.rb", "db/migrate/create_votes.rb"
+    end
+  end # VotesGenerator
+end # VotingEngine
