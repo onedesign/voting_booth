0608a460dee5e5743264f1aa138e956f414353c1
Dan Hodos <danhodos@gmail.com>
2011-02-05 11:15:57 -0600

    Adds ability to check if a votable was voted by a voter

--- lib/voting_engine/votable.rb
+++ lib/voting_engine/votable.rb
@@ -4,11 +4,19 @@ module VotingEngine
 
     module ClassMethods
       def votable(opts={})
+        include VotableInstanceMethods
+
         has_many :votes, :as => :votable
         has_many :votes_for, :as => :votable, :conditions => {:for => true}, :class_name => "Vote"
         has_many :votes_against, :as => :votable, :conditions => {:for => false}, :class_name => "Vote"
       end
     end # ClassMethods
+
+    module VotableInstanceMethods
+      def voted_by?(voter)
+        votes.for_voter(voter).first.present?
+      end
+    end # VotableInstanceMethods
   end # Votable
 end # VotableEngine
 
--- spec/models/blog_post_spec.rb
+++ spec/models/blog_post_spec.rb
@@ -4,4 +4,22 @@ describe BlogPost do
   it { should have_many(:votes) }
   it { should have_many(:votes_for) }
   it { should have_many(:votes_against) }
+
+  context "voted on" do
+    let(:voter) { mock_model(User) }
+    let(:vote) { mock_model(Vote) }
+    let(:vote_proxy) { mock('vote proxy') }
+
+    before { subject.stub(:votes).and_return(vote_proxy) }
+
+    it "should return true if the voter voted on this" do
+      vote_proxy.should_receive(:for_voter).with(voter).and_return([vote])
+      subject.voted_by?(voter).should be_true
+    end
+
+    it "should return false if the voter did not vote on this" do
+      vote_proxy.should_receive(:for_voter).with(voter).and_return([])
+      subject.voted_by?(voter).should be_false
+    end
+  end
 end
